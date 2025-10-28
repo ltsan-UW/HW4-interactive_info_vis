87cce6b495d82ae93e1c748e7c23682e77cd4d44
ltsan-UW <ltsan30@uw.edu>
2025-10-27 20:10:18 -0700

    sketch 4a added clock line

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -11,13 +11,15 @@ registerSketch('sk2', function (p) {
     p.textAlign(p.CENTER, p.CENTER);
     p.text('HWK #4. A', p.width / 2, p.height / 2);
     p.ellipse(p.mouseX, p.mouseY - 50, 50,50);
-    drawPie(p.windowWidth / 2, p.windowHeight / 2, 150, 0.5, 'green', 'yellow');
+
+
+    drawPie(p.windowWidth / 2, p.windowHeight / 2, 150, 0.5, 0.4, 'green', 'yellow');
   };
 
   p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
 
 
-  function drawPie(x, y, radius, percent, color1, color2) {
+  function drawPie(x, y, radius, percent, linePercent, color1, color2) {
     percent = p.constrain(percent, 0, 1);
 
     let angle1 = percent * 360;
@@ -30,9 +32,18 @@ registerSketch('sk2', function (p) {
     p.fill(color2);
     p.arc(x, y, radius * 2, radius * 2, angle1, 360, p.PIE);
 
+    let markerAngle = linePercent * 360;
+    let markerRad = p.radians(markerAngle);
+    let lineX = x + Math.cos(markerRad) * radius;
+    let lineY = y + Math.sin(markerRad) * radius;
+    p.stroke(0);
+    p.strokeWeight(3);
+    p.line(x, y, lineX, lineY);
+
     p.fill(0);
+    p.strokeWeight(1);
     p.textAlign(p.CENTER, p.CENTER);
     p.textSize(24);
-    p.text(`${percent * 100}%`, labelX, labelY);
+    p.text(`${percent * 100}%`, x, y - radius);
   }
 });
