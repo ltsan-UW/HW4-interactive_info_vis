53fcf11ec352c264318c8bb5dd01a37db983bf1d
ltsan-UW <ltsan30@uw.edu>
2025-10-29 15:45:44 -0700

    hw5 added lines that go to the draft ranking

--- sketches/sketch5.js
+++ sketches/sketch5.js
@@ -1,11 +1,24 @@
 // Example 2
 registerSketch('sk5', function (p) {
-  p.setup = function () {
-    p.createCanvas(p.windowWidth, p.windowHeight);
+
+  const draftPlayerMap = new Map();
+
+  let boardData;
+  let draftData;
+
+  p.preload = function () {
+    boardData = p.loadTable('sketches/hw5assets/ESPN-2024.csv', 'csv', 'header');
+    draftData = p.loadTable('sketches/hw5assets/DRAFT-2024.csv', 'csv', 'header');
   };
 
-    let boardData = p.loadTable('sketches/hw5assets/DRAFT-2024.csv', 'csv', 'header');
-    let draftData = p.loadTable('sketches/hw5assets/ESPN-2024.csv', 'csv', 'header');;
+  p.setup = function () {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+
+    for(let i = 0; i < draftData.getRowCount(); i++) {
+      draftPlayerMap.set(draftData.getString(i, "Player"), draftData.getString(i, "Pk"));
+    }
+  };
+
 
   p.draw = function () {
     let width = p.windowWidth;
@@ -18,16 +31,30 @@ registerSketch('sk5', function (p) {
 
     p.background(250);
 
+
+
     let rankRange = [1,30];
     let yStart = midHeight - graphHeight / 2;
     let yEnd = midHeight + graphHeight / 2;
     let distanceBetweenRanks = 3; // graphLength / number
+    let textPixelLineOffset = 10;
+
     p.fill(100, 150, 240);
     p.textSize(14);
     p.textAlign(p.CENTER, p.CENTER);
     for(let i = 1; i <= rankRange[1]; i++) {
+      let x = midWidth - graphLength / distanceBetweenRanks;
+      let x2 = midWidth + graphLength / distanceBetweenRanks;
       let y = p.map(i, rankRange[0], rankRange[1], yStart, yEnd)
-      p.text(i, midWidth - graphLength / distanceBetweenRanks, y);
+
+      let playerData = boardData.getRow(i);
+      playerData = playerData.obj;
+      let playerDraftPosition = draftPlayerMap.get(playerData.Name);
+      console.log(playerDraftPosition);
+
+      let y2 = p.map(playerDraftPosition, rankRange[0], rankRange[1], yStart, yEnd)
+      p.text(i, x, y);
+      p.line(x + textPixelLineOffset, y, x2 - textPixelLineOffset, y2)
     }
 
 
