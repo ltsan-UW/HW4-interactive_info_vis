12188a94a48ee2f440af026d863d84159b88805a
ltsan-UW <ltsan30@uw.edu>
2025-10-29 15:44:19 -0700

    hw5 added rankings on left side

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const SKETCH_SCRIPT_BY_ID = {
     sk14: 'sketches/sketch14.js',
 };
 
-window.DEFAULT_SKETCH = 'sk2';
+window.DEFAULT_SKETCH = 'sk5';
 // Default sketch selection logic:
 // 1) URL query `?tab=sk3` or `?tab=tab3` will select that tab on load
 // 2) or set `window.DEFAULT_SKETCH = 'sk2'` in the page before this script to choose a default
--- sketches/sketch5.js
+++ sketches/sketch5.js
@@ -4,22 +4,40 @@ registerSketch('sk5', function (p) {
     p.createCanvas(p.windowWidth, p.windowHeight);
   };
 
+    let boardData = p.loadTable('sketches/hw5assets/DRAFT-2024.csv', 'csv', 'header');
+    let draftData = p.loadTable('sketches/hw5assets/ESPN-2024.csv', 'csv', 'header');;
+
   p.draw = function () {
+    let width = p.windowWidth;
+    let height = p.windowHeight;
+    let midWidth = p.windowWidth / 2;
+    let midHeight = p.windowHeight / 2;
+
+    let graphLength = 800;
+    let graphHeight = 600;
+
     p.background(250);
 
-    // Corner time display
-    const h = p.hour();
-    const m = p.minute();
-    const s = p.second();
-    const label = p.nf(h, 2) + ':' + p.nf(m, 2) + ':' + p.nf(s, 2);
+    let rankRange = [1,30];
+    let yStart = midHeight - graphHeight / 2;
+    let yEnd = midHeight + graphHeight / 2;
+    let distanceBetweenRanks = 3; // graphLength / number
+    p.fill(100, 150, 240);
+    p.textSize(14);
+    p.textAlign(p.CENTER, p.CENTER);
+    for(let i = 1; i <= rankRange[1]; i++) {
+      let y = p.map(i, rankRange[0], rankRange[1], yStart, yEnd)
+      p.text(i, midWidth - graphLength / distanceBetweenRanks, y);
+    }
 
-    p.noStroke();
-    p.fill(20);
-    p.textAlign(p.LEFT, p.TOP);  // change to RIGHT/BOTTOM for other corners
-    p.textSize(50);
-    p.text(label, 12, 10);   // top-left corner
 
   }
+
+
+  function drawLine() {
+
+  }
+
 
   p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
 });
